93f37223275dd2334825d8bc84febec2a02cbc2a
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2023-02-20 01:46:40 +0530

    Added user registration and user login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    UserRegistrationComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent }
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'user-registration', component: UserRegistrationComponent },
+      { path: 'user-login', component: UserLoginComponent }
     ])
   ],
   providers: [],
